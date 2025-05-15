17e66117fe9c29298620e24fc16edf4372ee7f4c
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-05-15 21:36:05 +0300

    user schema model

---
+++ models/user.model.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  passwordHash: { type: String, required: true },
+  role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
+  createdAt: { type: Date, default: Date.now },
+}, { timestamps: true });
+
+export default mongoose.model('User', userSchema);
