3ee72a847f018aa22fdcc8e66272e945c72e8fb7
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-05-18 13:42:42 +0300

    initial setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Server-side code running...");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Server is running on port ${PORT}`);   
+});
