06ff749350703a6546da0f720b41e992887b7baf
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-05-27 00:02:11 +0300

    finilized the model for wishlist

--- models/wishlist.model.js
+++ models/wishlist.model.js
@@ -1,13 +1,37 @@
 import mongoose from 'mongoose';
 
 const wishlistSchema = new mongoose.Schema({
-  user: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User' },
-  products: [{
-     type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product' 
-    }],
-}, { timestamps: true });
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    products: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        addedAt: {
+            type: Date,
+            default: Date.now
+        },
+        notifyOnSale: {
+            type: Boolean,
+            default: false
+        },
+        notifyOnStock: {
+            type: Boolean,
+            default: false
+        }
+    }]
+}, {
+    timestamps: true
+});
 
-export default mongoose.model('Wishlist', wishlistSchema);
+// Create compound index to ensure a product can only be added once per user
+wishlistSchema.index({ user: 1, 'products.product': 1 }, { unique: true });
+
+const Wishlist = mongoose.model('Wishlist', wishlistSchema);
+
+export default Wishlist;
